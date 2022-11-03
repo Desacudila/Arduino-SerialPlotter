ff1a17f9221891c9c4f0338c49bcdb2098385b82
Fabrizio Desancio <desaniof@gmail.com>
2022-11-03 10:47:17 -0300

    first commit1

---
+++ Osciloscopio_Simple/Osciloscopio_Simple.ino
@@ -0,0 +1,15 @@
+void setup(){
+  pinMode(6, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  int analogValue;
+  float voltage;
+  
+  analogWrite(6, 127);
+  analogValue = analogRead(A0);
+  voltage = analogValue/204.6;
+
+  Serial.println(voltage);
+}
